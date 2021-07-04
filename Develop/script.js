c259f70c88d82c6615409fdd7d33e976a17a809d
LuisRetana <luisretana123@gmail.com>
2021-07-04 17:08:14 -0400

    added len and low variable

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,17 @@ let generatePassword = function(){
 	let passwordCharacters = ""
 	
 	let password = ""
+  
+  let len = parseInt(window.prompt("Enter the length of the password (8 - 128): "));
+	while (len > 128 || len < 8){
+		len = window.prompt("Entered invalid number, please enter the length of the password (8 - 128): ");
+	}
+
+	let low = window.prompt("Use lowercase? ('y' or 'n'): ").toLowerCase();
+	while (low != "y" && low != "n"){
+		low = window.prompt("Entered invalid input, Use lowercase? ('y' or 'n'): ").toLowerCase();
+	}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
